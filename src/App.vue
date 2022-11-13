7feeafea00a95a32dd0eb64d414593b629361f80
Smidra <rad.smid@gmail.com>
2022-11-13 21:04:01 +0100

    New version with DaisyUI

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <h1>Datoun katalog</h1>
+  <div class="text-center font-sans text-6xl font-semibold
+   text-sky-500 p-10">
+    Datoun katalog
+  </div>
+
   <ais-instant-search :search-client="searchClient" index-name="firmy">
 
-  <div class="left-panel">
-    <h2>Kategorie</h2>
-    <ais-menu attribute="vyrobny.kategorie"/>
-    <ais-configure :hitsPerPage="8" />
-  </div>
+    <!-- <h2>Kategorie</h2>
+  <ais-menu attribute="vyrobny.kategorie" /> -->
 
-  <div class="right-panel">
-    <ais-search-box />
-    <ais-hits>
-      <template v-slot:item="{ item }">
-        <h2>{{ item.jmeno_firmy }} / {{ item.vyrobny[0].lokalita }}</h2>
-        {{ item.popisek_firmy }}
-        <p><a :href=item.eshop>{{ item.eshop }}</a></p>
-      </template>
-    </ais-hits>
-    <ais-pagination />
-  </div>
+    <!-- class="w-10 mx-20 my-4 p-4 rounded-lg border-2 border-indigo-500 hover:shadow-xl" -->
 
-</ais-instant-search>
+    <ais-search-box class="md:w-3/4 lg:w-1/2 mx-auto p-2" :class-names="{
+      'ais-SearchBox-input': 'p-6 w-full rounded-lg border-2 border-indigo-500 hover:shadow-xl',
+      'ais-SearchBox-submitIcon': 'hidden',
+      'ais-SearchBox-resetIcon': 'hidden',
+    }" type="text" name="search" placeholder="Hlodej šmudlo..." />
+
+    <div class="p-4"></div>
+    <!-- <div
+      class="md:w-3/4 lg:w-1/2 mx-auto p-2 grid w-11/12 p-6 mx-auto grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 "> -->
+    <div class="flex flex-row w-3/4 mx-auto">
+      
+      <div class="basis-1/4">
+        <ais-menu class="menu bg-base-100 w-56 p-2 rounded-box" attribute="vyrobny.kategorie" />
+      </div>
+
+      <ais-hits class="basis-3/4">
+
+        <template v-slot:item="{ item }">
+          <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box p-1">
+            <div class="collapse-title text-xl font-medium">
+              {{ item.jmeno_firmy }} / {{ item.vyrobny[0].lokalita }}
+            </div>
+            <div class="collapse-content"> 
+              <p>{{ item.popisek_firmy }}<a :href=item.eshop>{{ item.eshop }}</a></p>
+            </div>
+          </div>
+        </template>
+      </ais-hits>
+
+    </div>
+
+  </ais-instant-search>
 </template>
 
 <script>
 import { instantMeiliSearch } from '@meilisearch/instant-meilisearch';
-import 'instantsearch.css/themes/algolia.css';
-
 
 export default {
   data() {
@@ -39,21 +59,3 @@ export default {
   },
 };
 </script>
-
-<style>
-body {
-  font-family: sans-serif;
-  padding: 1em;
-}
-
-.ais-Hits-list {
-  margin-top: 0;
-  margin-bottom: 1em;
-}
-
-.ais-InstantSearch {
-  display: grid;
-  grid-template-columns: 1fr 4fr;
-  grid-gap: 1em;
-}
-</style>
