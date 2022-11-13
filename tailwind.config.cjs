d739b5c7d857f5fb0ccb9f5445b2551d87d701c3
Smidra <rad.smid@gmail.com>
2022-11-13 23:21:32 +0100

    DaisyUI themes.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "corporate",
+    ],
+  },
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
