05bbee4056d9f84440ee7c08c40e8c6474021c76
Smidra <rad.smid@gmail.com>
2022-12-01 22:16:50 +0100

    Background is now gray.

--- src/App.vue
+++ src/App.vue
@@ -10,42 +10,44 @@ const searchClient = algoliasearch('S27OT8U78J', '6c71ae31ec984a2185234a501482d5
 </script>
 
 <template>
-  <!-- Heading -->
-  <n-h1 class="text-center text-5xl p-6 md:text-6xl md:pt-10">
-    <a href="/">Datoun</a>
-  </n-h1>
+  <div class="min-h-screen bg-gray-100">
+    <!-- Heading -->
+    <n-h1 class="text-center text-5xl p-6 md:text-6xl md:pt-10">
+      <a href="/">Datoun</a>
+    </n-h1>
 
-  <ais-instant-search :search-client="searchClient" index-name="firmy">
-    <!-- Search box -->
-    <SearchBoxComponent />
-    <!-- Vertical spacing -->
-    <div class="p-3"></div>
+    <ais-instant-search :search-client="searchClient" index-name="firmy">
+      <!-- Search box -->
+      <SearchBoxComponent />
+      <!-- Vertical spacing -->
+      <div class="p-3"></div>
 
-    <!-- NORMAL Menu is left and results are right -->
-    <div class="flex flex-row mx-auto w-full md:w-3/4">
-      <!-- LEFT is menu -->
-      <div class="w-0 invisible md:visible md:w-1/4">
-        <MenuComponent />
-        <!-- <Statistics /> -->
+      <!-- NORMAL Menu is left and results are right -->
+      <div class="flex flex-row mx-auto w-full md:w-3/4">
+        <!-- LEFT is menu -->
+        <div class="w-0 invisible md:visible md:w-1/4">
+          <MenuComponent />
+          <!-- <Statistics /> -->
+        </div>
+        <!-- RIGHT are results -->
+        <div class="mb-52 w-full px-2 md:p-0 md:w-3/4">
+          <ResultsComponent />
+        </div>
       </div>
-      <!-- RIGHT are results -->
-      <div class="w-full px-2 md:p-0 md:w-3/4">
-        <ResultsComponent />
-      </div>
-    </div>
 
-    <!-- MOBILE Menu becomes a button on mobile screens -->
-    <!-- <div class="z-20 dropdown dropdown-top flex justify-center"> -->
-    <!-- Button. Click to close copied form https://github.com/saadeghi/daisyui/issues/157 -->
-    <!-- <label tabindex="0" class="btn btn-accent btn-circle px-20 fixed bottom-2 md:invisible"
+      <!-- MOBILE Menu becomes a button on mobile screens -->
+      <!-- <div class="z-20 dropdown dropdown-top flex justify-center"> -->
+      <!-- Button. Click to close copied form https://github.com/saadeghi/daisyui/issues/157 -->
+      <!-- <label tabindex="0" class="btn btn-accent btn-circle px-20 fixed bottom-2 md:invisible"
         onclick="this.parentElement.classList.toggle('dropdown-open');document.activeElement.blur()">Kategorie</label> -->
-    <!-- Opened menu -->
-    <!-- <div class="fixed bottom-16 flex justify-center w-full">
+      <!-- Opened menu -->
+      <!-- <div class="fixed bottom-16 flex justify-center w-full">
         <ul tabindex="0" class="dropdown-content bg-base-200 rounded-box p-6 w-full">
           <MenuComponent />
         </ul>
       </div>
     </div> -->
 
-  </ais-instant-search>
+    </ais-instant-search>
+  </div>
 </template>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-3 border rounded mx-3">
+    <div class="p-3 border rounded mx-3 bg-white ">
         <n-h2 class="pt-1">Kategorie</n-h2>
         <ais-hierarchical-menu class="dark:text-base-content menu text-primary" :show-parent-level="true"
             :attributes="['vyrobny.kategorie0', 'vyrobny.kategorie1', 'vyrobny.kategorie2']">
